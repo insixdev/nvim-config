67d7c4e4662107d7894fc758c94264fe390a2a7e
insidevArch <insidev.12@gmail.com>
2025-09-03 03:19:20 +0000

    Version con un override sobre puesto, mas adelante hacer mis temas

--- lua/options.lua
+++ lua/options.lua
@@ -7,3 +7,7 @@ vim.wo.relativenumber = true
 vim.opt.termguicolors = false
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.g.neovide_font = "Hack:h12"  -- Fuente y tamaño
+vim.g.neovide_antialiasing = true
+vim.g.neovide_cursor_antialiasing = true
