d0972db47615fabd9d8e89b0dd618473efc655a3
insidevArch <insi.dev12@gmail.com>
2025-09-20 02:13:25 -0300

    fix: full path in lualine

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -33,7 +33,7 @@ return {
       sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { { "filename", path = 1 } },
+        lualine_c = { { "filename", path = 2 }, { "cwd", color = { fg = "#a0a0f0"}} },
         lualine_x = { "encoding", "fileformat", "filetype" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
