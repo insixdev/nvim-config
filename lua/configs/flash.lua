6bfe79cbff869b49ce9bacff4704e2ad5e3d371c
insidevArch <insi.dev12@gmail.com>
2025-09-23 00:26:19 -0300

    fix: flash keybind s to f

--- lua/configs/flash.lua
+++ lua/configs/flash.lua
@@ -9,7 +9,7 @@ return {
     },
   },
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Jump" },
+    { "b", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Jump" },
     { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
