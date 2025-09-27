765a40d7c20e282ffc4270884ee9ceb07d7b9363
insidevArch <insi.dev12@gmail.com>
2025-09-27 00:00:36 -0300

    fix(oil): mapping change

--- lua/configs/oil.lua
+++ lua/configs/oil.lua
@@ -83,7 +83,7 @@ return {
         ["-"] = { "actions.parent", mode = "n" },
         ["0"] = { "actions.open_cwd", mode = "n" },
         ["'"] = { "actions.cd", mode = "n" },
-        ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+        ["¿"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
         ["gs"] = { "actions.change_sort", mode = "n" },
         ["gx"] = "actions.open_external",
         ["g."] = { "actions.toggle_hidden", mode = "n" },
