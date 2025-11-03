c0fcf53178c4a4066df23e379ca3b7c02e8dcea1
insidevArch <insi.dev12@gmail.com>
2025-11-03 18:11:56 -0300

    fix: autosave fix trouble with lspsaga
    exclude buffer without name

--- lua/configs/auto-save.lua
+++ lua/configs/auto-save.lua
@@ -10,11 +10,19 @@ return {
       execution_message = { message = function() return "" end },
       trigger_events = {"InsertLeave", "TextChanged"},
       condition = function(buf)
+        -- excluir buffer sin nombr
+        if not vim.api.nvim_buf_is_valid(buf) then
+          return false
+        end
         local ft = vim.bo[buf].filetype
         local bt = vim.bo[buf].buftype
 
         -- excluir Oil y otros buffers especiales
         if ft == "oil" or ft == "gitcommit" or bt == "nofile" then
+          return false
+        end
+        -- evitar que este en buffer de lsp-saga finder
+        if ft == "sagafinder" then
           return false
         end
 
