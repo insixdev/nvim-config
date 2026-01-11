2debb42bb9f0ecbec9562eecad3ccdaf31737018
insidevArch <insi.dev12@gmail.com>
2026-01-11 14:26:00 -0300

    fix: better  godotsupport

--- lua/configs/comments.lua
+++ lua/configs/comments.lua
@@ -71,6 +71,7 @@ return {
         WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
         PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
         NOTE = { icon = " ", color = "#ffcc66" },
+        CONV= { icon = " ", color = "#cc6c66" },
         TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
       },
     }
--- lua/options.lua
+++ lua/options.lua
@@ -223,6 +223,7 @@ vim.opt.guicursor = ""
 vim.api.nvim_create_autocmd("ColorScheme", {
   pattern = {
     "kanso",
+    "ayu",
     "kanso-zen",
     "kanso-mist",
     "kanso-pearl",
@@ -368,26 +369,24 @@ require('fff').setup({
     }
 })
 
-local paths_to_check = {'/', '/../'}
-local is_godot_project = false
-local godot_project_path = ''
 local cwd = vim.fn.getcwd()
--- iterate over paths and check
-for key, value in pairs(paths_to_check) do
-    if vim.uv.fs_stat(cwd .. value .. 'project.godot') then
-        is_godot_project = true
-        godot_project_path = cwd .. value
-        break
+local project_file = cwd .. '/project.godot'
+local is_godot_project = vim.uv.fs_stat(project_file) ~= nil
+local godot_project_path = cwd
+
+if is_godot_project then
+    local server_pipe = godot_project_path .. '/server.pipe'
+    local is_server_running = vim.uv.fs_stat(server_pipe) ~= nil
+
+    if not is_server_running then
+        vim.fn.serverstart(server_pipe)
+        print("Godot server started at " .. server_pipe)
+    else
+        print("Godot server already running")
     end
+else
+  -- not a godot proyect
 end
-
--- check if server is already running in godot project path
-local is_server_running = vim.uv.fs_stat(godot_project_path .. '/server.pipe')
--- start server, if not already running
-if is_godot_project and not is_server_running then
-    vim.fn.serverstart(godot_project_path .. '/server.pipe')
-end
-
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "oil",
   callback = function(args)
